7a04532470b52bef4aeae2f373145da5949e358e
Jacob Krall <jkrall@netflix.com>
2023-12-01 15:25:02 -0500

    1.1: Output 3-byte BCD

--- hellorld.s
+++ hellorld.s
@@ -12,6 +12,8 @@ PTR = $10
 FIRST_DIG = $12
 LAST_DIG = $13
 SUM = $14
+HTD_IN = SUM
+HTD_OUT = $16
 OUT = $F000
 
 init:
@@ -90,9 +92,12 @@ endl:
 	jmp do_line
 
 exit:
-	lda SUM
+	jsr htd
+	lda HTD_OUT
 	sta OUT
-	lda SUM+1
+	lda HTD_OUT+1
+	sta OUT
+	lda HTD_OUT+2
 	sta OUT
 	brk
 
@@ -107,6 +112,53 @@ next_ch:
 	sta PTR+1
 	pla
 	rts
+
+; From http://www.6502.org/source/integers/hex2dec.htm
+; (which is why it uses __H suffix syntax)
+TABLE:	.byte    0, 0H, 1H,  0, 0H, 2H,  0, 0H, 4H,  0, 0H, 8H
+	.byte    0, 0H,16H,  0, 0H,32H,  0, 0H,64H,  0, 1H,28H
+	.byte    0, 2H,56H,  0, 5H,12H,  0,10H,24H,  0,20H,48H
+	.byte    0,40H,96H,  0,81H,92H,  1,63H,84H,  3,27H,68H
+htd:
+	; Zero HTD_OUT
+	sty HTD_OUT
+	sty HTD_OUT+1
+	sty HTD_OUT+2
+
+	; Save registers A and X
+	pha
+	txa
+	pha
+
+	ldx #15 + 15 + 15 ; 3x15 bits.
+	sed
+htd_loop:
+	asl HTD_IN      ; (0 to 15 is 16 bit positions.)
+	rol HTD_IN+1    ; If the next highest bit was 0,
+	bcc htd1       ; then skip to the next bit after that.
+	lda HTD_OUT     ; But if the bit was 1,
+	clc             ; get ready to
+	adc TABLE+2,X   ; add the bit value in the table to the
+	sta HTD_OUT     ; output sum in decimal--  first low byte,
+	lda HTD_OUT+1   ; then middle byte,
+	adc TABLE+1,X
+	sta HTD_OUT+1
+	lda HTD_OUT+2   ; then high byte,
+	adc TABLE,X     ; storing each byte
+	sta HTD_OUT+2   ; of the summed output in HTD_OUT.
+
+htd1:	dex             ; By taking X in steps of 3, we don't have to
+	dex             ; multiply by 3 to get the right bytes from the
+	dex             ; table.
+	bpl htd_loop
+
+	; Restore registers A and X
+	pla
+	tax
+	pla
+	cld
+	rts
+
 
 data: .incbin "input"
 	.byte 0
